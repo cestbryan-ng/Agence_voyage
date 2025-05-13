94cf353b3e2ecce9b1ffade0078a6bbbb0fb7539
cestbryan_ng <ngoupeyoubryan9@gmail.com>
2025-05-13 21:30:38 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
+
+
+
+
 
 const App = () => {
+  function Calculer() {
+    var nombre1 = parseFloat(texte), nombre2 = parseFloat(texte1);
+    if (isNaN(nombre1) || isNaN(nombre2)) {
+      setresultat("Erreur: Veuillez entrer des nombres valides.");
+    } else {
+      setresultat(nombre1 + nombre2);
+    }
+  }
+
+  const [texte, settexte] = useState("");
+  const [texte1, settexte1] = useState("");
+  const [resultat, setresultat] = useState("");
+
   return (
     <View style={styles.container}>
-      <Button style = {styles.text} title="Appuie ici" onPress={() => alert("Hello World !")} />
+      <TextInput defaultValue = {texte} onChangeText = {newtexte => settexte(newtexte)}
+      placeholder = "Entrer un nombre..." />
+            <TextInput defaultValue = {texte1} onChangeText = {newtexte => settexte1(newtexte)}
+      placeholder = "Entrer un deuxième nombre..." />
+      <Button title = "Calculer" onPress = {Calculer}  />
+      <Text>{resultat}</Text>
     </View>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'black',
-  },
-  text: {
-    fontSize: 24,
-    color: 'red',
+    backgroundColor: '#F75D37',
   },
 });
 
